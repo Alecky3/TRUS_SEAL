6c210a95844fb025079984bf9f501f47b9bd96f2
alecky3 <alexmuthini3@gmail.com>
2024-11-23 22:07:45 +0300

    updated Business and Seal Models

--- TrustSeal/Migrations/20241123001451_ApplicationTracking_IsMain.Designer.cs
+++ TrustSeal/Migrations/20241123001451_ApplicationTracking_IsMain.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TrustSeal.Areas.Identity.Data;
 
@@ -11,9 +12,11 @@ using TrustSeal.Areas.Identity.Data;
 namespace TrustSeal.Migrations
 {
     [DbContext(typeof(TSAuth))]
-    partial class TSAuthModelSnapshot : ModelSnapshot
+    [Migration("20241123001451_ApplicationTracking_IsMain")]
+    partial class ApplicationTracking_IsMain
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -257,7 +260,7 @@ namespace TrustSeal.Migrations
                     b.Property<bool>("NeedsAttention")
                         .HasColumnType("bit");
 
-                    b.Property<int>("Order")
+                    b.Property<int?>("Order")
                         .HasColumnType("int");
 
                     b.Property<bool>("Required")
@@ -386,9 +389,6 @@ namespace TrustSeal.Migrations
 
                     b.Property<int?>("SealId")
                         .HasColumnType("int");
-
-                    b.Property<string>("SealReadableId")
-                        .HasColumnType("nvarchar(max)");
 
                     b.Property<string>("Status")
                         .HasColumnType("nvarchar(max)");
