e065a2ae49049705dfdeb21a624f67afb29f8b5e
Alecky3 <alexmuthini3@gmail.com>
2025-07-27 22:55:18 +0300

    added embed HTLM code

--- TrustSeal/Pages/TsSeals/index.cshtml.cs
+++ TrustSeal/Pages/TsSeals/index.cshtml.cs
@@ -44,17 +44,20 @@ namespace TrustSeal.Pages.TsSeals
             public Guid SealCode {get;set;}
         }
         public IList<AllSeals> Seals {get;set;} = new List<AllSeals>();
+        public string Hostname { get; set; }
 
         public async Task OnGet()
         {
-             TSUser user = await _userManager.GetUserAsync(User);
+            var hostnameX = $"{Request.Scheme}://{Request.Host}";
+            Hostname = hostnameX;
+            TSUser user = await _userManager.GetUserAsync(User);
             if (user != null)
             {
                 var role = await _userManager.GetRolesAsync(user);
                 Seals = await _context.Seals
-                                                .Include(s=>s.Business)
-                                                .ThenInclude(b=>b.Owner)
-                                                .Where(s=>s.Business.OwnerId == user.Id)
+                                                .Include(s => s.Business)
+                                                .ThenInclude(b => b.Owner)
+                                                .Where(s => s.Business.OwnerId == user.Id)
                                                 .Select(s => new AllSeals
                                                 {
                                                     CreatedAt = s.CreatedAt,
@@ -62,10 +65,10 @@ namespace TrustSeal.Pages.TsSeals
                                                     BusinessLegalName = s.Business.LegalName,
                                                     SealReadableId = s.Business.SealReadableId,
                                                     SealCode = s.SealCode,
-                                                    Owner=s.Business.Owner
+                                                    Owner = s.Business.Owner
                                                 })
                                                 .ToListAsync();
-                
+
             }
         }
         public async Task<IActionResult> OnGetAllSealsAsync()
